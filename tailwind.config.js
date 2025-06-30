2b41a99779b28164abf86fd18ec1812f75db2b39
nnavyy <nandazhafran@gmail.com>
2025-06-30 16:02:33 +0700

    app version 1.2.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-// tailwind.config.js atau tailwind.config.ts
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
-      // extend warna, font, dsb
-    },
+      colors: {
+        border: 'oklch(0.922 0 0)', // bisa diganti sesuai kebutuhan
+        background: 'oklch(1 0 0)',
+        foreground: 'oklch(0.145 0 0)',
+        // tambahin warna lain sesuai isi css lo
+      }
+    }
   },
-  plugins: [
-    require("tailwindcss-animate"),
-  ],
+  plugins:[],
 }
